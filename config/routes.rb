8e15c88ee9e3885557444a89a964307b4d86cd6e
Ramprasad Ramachandran <rramachandran@greatschools.org>
2013-01-30 14:02:04 -0800

    Testing git commit. added login page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 H2::Application.routes.draw do
+  get "account/login"
+
+  get "sign_in/login"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ H2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'account#login'
 
   # See how all your routes lay out with "rake routes"
 
