c5a5e43730e0da38b1690ec0dc9fea0d596f2762
Mitchell Seltzer <mseltzer@greatschools.org>
2013-01-30 09:59:55 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+H2::Application.config.secret_token = '4f67bb994988ecbb01543483c4d64163cd804ea117714cbe09ac21549eb17782e38fe5cd14707940e507258b4c22ceefdde559d66ff16429250f517b7192aff0'
