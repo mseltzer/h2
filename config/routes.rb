f2ee12161088b9eb912bbc896800aa8747ffd40d
Mitchell Seltzer <mseltzer@greatschools.org>
2013-01-31 14:07:53 -0800

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -81,6 +81,7 @@ H2::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  match '/index', to: 'users#index'
   match '/login', to: 'users#login'
   match '/home', to: 'users#home'
 end
